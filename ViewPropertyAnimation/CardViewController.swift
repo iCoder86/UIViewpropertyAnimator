58cdaf05a51a7ae80aa0ffc84f295b1a02f62ed5
Mehul <mehul@cloudzon.com>
2018-11-16 19:30:57 +0530

    Initial commit

---
+++ ViewPropertyAnimation/CardViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CardViewController.swift
+//  ViewPropertyAnimation
+//
+//  Created by Mehul on 16/11/18.
+//  Copyright © 2018 Mehul. All rights reserved.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+
+    @IBOutlet weak var handleArea: UIView! 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
